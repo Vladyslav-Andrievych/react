1972018b941aa0fe3af516c5121bfd606de5c133
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-11 18:57:33 +0200

    lesson 1 task 1

---
+++ lesson1/task1/index.js
@@ -0,0 +1,11 @@
+const rootElem = document.querySelector('#root');
+
+const element = React.createElement(
+  'div',
+  {
+    className: 'greeting',
+  },
+  'Hello, React!'
+);
+
+ReactDOM.render(element, rootElem);
