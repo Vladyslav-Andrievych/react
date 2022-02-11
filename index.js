04fc5983f3d14820aff86814fab55cfb8714559d
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-11 19:14:00 +0200

    lesson 1 1 changed react version

--- index.js
+++
@@ -1,9 +0,0 @@
-const rootElem = document.querySelector('#root');
-
-const element = React.createElement(
-  'div',
-  { className: 'greeting' },
-  'Hello, React!'
-);
-
-ReactDOM.render(element, rootElem);
