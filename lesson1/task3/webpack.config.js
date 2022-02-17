c3ba75b747f813f4fc2fcbdeeb68d0eb3dcd7d9e
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-17 12:23:23 +0200

    lesson 1 task 3

---
+++ lesson1/task3/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.join(__dirname, 'review_build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
