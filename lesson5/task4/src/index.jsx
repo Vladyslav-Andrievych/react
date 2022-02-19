9de5bbc847d7e3354602b035a913a4f1b9d70ee2
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-19 18:12:43 +0200

    lesson 5 task 4

---
+++ lesson5/task4/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Colors from './Colors.jsx';
+
+ReactDOM.render(<Colors />, document.querySelector('#root'));
