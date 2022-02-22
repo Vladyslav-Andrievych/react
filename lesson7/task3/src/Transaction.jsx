8b6a59fb22fd675889f5b10ed95c8deff628d9a3
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-22 14:50:39 +0200

    lesson 7 task 3 (hometask 1)

---
+++ lesson7/task3/src/Transaction.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { formatDate, formatTime, formatNumbers } from './formatters.js';
+
+const User = ({ from, to, amount, rate, time }) => {
+  return (
+    <li className="transaction">
+      <span className="transaction__date">{formatDate(time)}</span>
+      <span className="transaction__time">{formatTime(time)}</span>
+      <span className="transaction__assets">
+        {from} → {to}
+      </span>
+      <span className="transaction__rate">{formatNumbers(rate)}</span>
+      <span className="transaction__amount">{formatNumbers(amount)}</span>
+    </li>
+  );
+};
+
+export default User;
