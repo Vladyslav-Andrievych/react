179d86a17bd2efd94034895b12609160dd925691
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-22 13:23:50 +0200

    lesson 7 task 1

---
+++ lesson7/task1/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NumbersList from './NumbersList.jsx';
+
+const numbers = [1, 2, 3, 4, 5];
+
+const App = () => {
+  return <NumbersList numbers={numbers} />;
+};
+
+export default App;
