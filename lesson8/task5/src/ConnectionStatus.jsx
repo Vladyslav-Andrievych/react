0f209342d9c23580fc43dc73caea381bf6cb2de7
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-23 20:27:24 +0200

    lesson 8 task 5 (hometask 1) change text output

--- lesson8/task5/src/ConnectionStatus.jsx
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -30,7 +30,7 @@ class ConnectionStatus extends Component {
   render() {
     return (
       <div className={this.state.isOnline ? 'status' : 'status status_offline'}>
-        {this.state.isOnline ? 'Online' : 'Offline'}
+        {this.state.isOnline ? 'online' : 'offline'}
       </div>
     );
   }
