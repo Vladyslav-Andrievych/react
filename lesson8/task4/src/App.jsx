61bb1cfbf84639869984782776b4322abbfe45e4
Vladyslav Andrievych <vladandrievych@gmail.com>
2022-02-23 20:01:08 +0200

    lesson 8 task 4

---
+++ lesson8/task4/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import User from './User.jsx';
+
+class App extends Component {
+  render() {
+    return <User userId="facebook" />;
+  }
+}
+
+export default App;
