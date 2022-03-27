2b36c46bf07d10bc17285e9917a68371c4313783
Vladyslav-Andrievych <vladandrievych@gmail.com>
2022-03-27 14:28:14 +0300

    lesson 9 task 5 tic-tac-toe game

---
+++ lesson9/task5/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Game from './Game.jsx';
+import './index.scss';
+
+const rootElem = document.querySelector('#root');
+
+ReactDOM.render(<Game />, rootElem);
