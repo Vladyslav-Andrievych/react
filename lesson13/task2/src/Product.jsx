90e70140ae01cfb9b8671b6cb0abfe5179b42b4d
Vladyslav-Andrievych <vladandrievych@gmail.com>
2022-04-06 13:09:47 +0300

    lesson 13 task 2

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const Product = () => {
-  const { productId } = useParams();
-  return <div className="product">{`Product is a ${productId}`}</div>;
+const Product = ({ match }) => {
+  return (
+    <div className="product">{`Product is a ${match.params.productId}`}</div>
+  );
 };
 
 export default Product;
