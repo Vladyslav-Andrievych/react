2b3a406c2f3aa15952e238b7958cce136e7fea71
Vladyslav-Andrievych <vladandrievych@gmail.com>
2022-04-10 12:08:18 +0300

    lesson 14 task 4

---
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import classNames from 'classnames';
+
+const ConnectionStatus = () => {
+  const [isOnline, setIsOnline] = useState(true);
+
+  useEffect(() => {
+    function handleStatusChange(event) {
+      const isOnline = event.type === 'online';
+      setIsOnline(isOnline);
+    }
+
+    window.addEventListener('online', handleStatusChange);
+    window.addEventListener('offline', handleStatusChange);
+
+    return () => {
+      window.removeEventListener('online', handleStatusChange);
+      window.removeEventListener('offline', handleStatusChange);
+    };
+  }, []);
+
+  return (
+    <div className={classNames('status', { status_offline: !isOnline })}>
+      {isOnline ? 'Online' : 'Offline'}
+    </div>
+  );
+};
+
+export default ConnectionStatus;
